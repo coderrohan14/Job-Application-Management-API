1421116172b400cfdc5d851f98d0ba2b0f019051
coderrohan14 <Rahkins@1996>
2023-01-22 18:57:28 +0530

    package changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 // extra security packages
 const helmet = require("helmet")
 const cors = require("cors")
-const xss = require("xss") //cross site scripting
+const xss = require("xss-clean") //cross site scripting
 const rateLimiter = require("express-rate-limit")
 
 
@@ -38,7 +38,7 @@ app.use(
 app.use(express.json());
 app.use(helmet())
 app.use(cors())
-app.use(xss)
+app.use(xss())
 
 app.get("/", (req, res)=>{
   res.send('<h1>Jobs Application Management API</h1><a href="/api-docs">Documentation</a>')
